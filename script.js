e242a2ddf4d7ad7cb78fae7c760fe95cf2b5ceb0
kirillprokhorenko <kirillprokhorenko@proton.me>
2023-07-15 22:30:19 +0300

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return 'ERR0R';
+    }
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
