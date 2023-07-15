8947ba9c2f8478d61225aa22324ff1620ec34a22
kirillprokhorenko <kirillprokhorenko@proton.me>
2023-07-15 22:45:39 +0300

    Add function that manages operations

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function substract(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        return 'ERR0R';
+        return 'ERR0R: Division by zero';
     }
     return a / b;
 }
@@ -20,3 +20,26 @@ function multiply(a, b) {
 let first = null;
 let second = null;
 let operator = null;
+
+function parseOperation(operation) {
+    operator = operation.replace(/[0-9]/g, '');
+    const index = operation.indexOf(operator);
+    first = +(operation.slice(0, index));
+    second = +(operation.slice(index + 1));
+}
+
+function operate(operation) {
+    parseOperation(operation);
+    switch (operator) {
+        case '+':
+            return add(first, second);
+        case '-':
+            return substract(first, second);
+        case '/':
+            return divide(first, second);
+        case '*':
+            return multiply(first, second);
+        default:
+            return 'ERR0R: Unknown operator';
+    }
+}
