08efb055e294335ab8cd32353025b5fee1c430a0
kirillprokhorenko <kirillprokhorenko@proton.me>
2023-07-17 21:28:43 +0300

    Change function for managing operations and operation variables

--- script.js
+++ script.js
@@ -17,29 +17,20 @@ function multiply(a, b) {
     return a * b;
 }
 
-let first = null;
-let second = null;
-let operator = null;
+let first = '';
+let second = '';
+let operator = '';
 
-function parseOperation(operation) {
-    operator = operation.replace(/[0-9]/g, '');
-    const index = operation.indexOf(operator);
-    first = +(operation.slice(0, index));
-    second = +(operation.slice(index + 1));
-}
-
-function operate(operation) {
-    parseOperation(operation);
+function operate(first, second, operator) {
     switch (operator) {
         case '+':
-            return add(first, second);
+            return add(+first, +second);
         case '-':
-            return substract(first, second);
+            return substract(+first, +second);
         case '/':
-            return divide(first, second);
+            return divide(+first, +second);
         case '*':
-            return multiply(first, second);
-        default:
-            return 'ERR0R: Unknown operator';
+            return multiply(+first, +second);
     }
 }
+
